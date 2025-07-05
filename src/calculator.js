c57056df65906b433d2a297be821bc3a2da94381
keval <kevalnpatel070@gmail.com>
2025-07-05 15:44:43 +0530

    feat: Support custom delimiter syntax like //;\n1;2

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,24 @@
+// Characters like *, +, ?, . aren't just characters in regex — they control how patterns behave
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
 
 function add(numbers) {
     if (numbers === '') return 0;
 
-    //replace new line with ',' and handle as comma seprated value handle
-    const numberArray = numbers.replace(/\n/g, ',').split(',').map(Number);
+    let delimiter = /,|\n/; 
+
+    if (numbers.startsWith('//')) {
+    
+        const delimiterLine = numbers.match(/^\/\/(.)\n/);
+        if (delimiterLine) {
+            delimiter = new RegExp(escapeRegExp(delimiterLine[1]));// Escape delimiter to handle special regex characters
+            numbers = numbers.slice(delimiterLine[0].length);
+        }
+
+    }
+
+    const numberArray = numbers.split(delimiter).map(Number);
     return numberArray.reduce((sum, num) => sum + num, 0);
 }
 
