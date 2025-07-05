4b9c85f43b4ec2ea004e155e2ac13620616f4949
keval <kevalnpatel070@gmail.com>
2025-07-05 14:56:16 +0530

    test: Failing test for empty string ''

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,12 @@
+//init
 describe('Sanity checks', () => {
     test('Testing environment is operational', () => {
         expect(1).toBe(1);
     });
 });
+
+describe('Add method tests', () => {
+    test('Returns 0 as sum for an empty string ', () => {
+        expect(add('')).toBe(0)
+    });// 1
+})
